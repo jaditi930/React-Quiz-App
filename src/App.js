d5eee90d3f846c5549d0c842ad3a1bf9849ba0ee
Aditi Jain <jaditi930@gmail.com>
2024-03-04 06:10:38 +0530

    quiz component done

--- src/App.js
+++ src/App.js
@@ -5,20 +5,34 @@ import './App.css'
 
 function App() {
 
-  const [questions,setQuestions] = useState([])
+  let questions_data = [
+    {
+        "question":"What is the capital of France ?",
+        "options":["Berlin","London","Brussels","Paris"],
+        "correct":"Paris"
+
+    },
+    {
+        "question":"What is the capital of India ?",
+        "options":["Indore","New Delhi","Chandigarh","Ahemdabad"],
+        "correct":"Paris"
+    }
+]
+
+  const [questions,setQuestions] = useState(questions_data)
 
   useEffect(()=>{
   
   console.log("useeffect")
 
-  fetch("http://localhost:3000/data.json")
+  // fetch("http://localhost:3000/data.json")
 
-  .then((response)=>response.json())
+  // .then((response)=>response.json())
 
-  .then((data)=>{
-    console.log(data)
-    setQuestions(data)
-  })
+  // .then((data)=>{
+  //   console.log(data)
+  //   setQuestions(data)
+  // })
 
   },[])
 
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,26 +3,10 @@ import React, { useState } from 'react'
 const Quiz = ({questions}) => {
 
   const [quesNo,setQuesNo] = useState(0)
-  const [isSelected,setSelected] = useState(false)
   const [optionNo,setOptionNo] = useState(-1)
   const [score,setScore] = useState(0)
-  const [showScore,setShowScore] = useState(false)
 
-  let current_question = questions[quesNo]
   console.log(questions)
-
-  let options = current_question.options.map((option,index)=>{
-
-    return (
-    <div className="option" key={index} onClick={()=>{ 
-    setOptionNo(index)
-    setSelected(true)
-    }}>  
-    {option}  
-    </div>
-    )
-
-  })
 
   function handleSubmit(){
 
@@ -31,32 +15,43 @@ const Quiz = ({questions}) => {
   return;
 
   // if marked option is correct
-  if(current_question.correct == current_question.options[optionNo])
+  if(questions[quesNo].correct == questions[quesNo].options[optionNo])
   setScore(score+1)
 
-  // if it was the last question then display score
-  if(quesNo == questions.length-1)
-  setShowScore(true)
 
   else{
     setQuesNo(quesNo+1)
     setOptionNo(-1)
-    setSelected(false)
   }
 
   }
 
   return (
+    <>
+    { quesNo < questions.length ? (
         <div className='ques'>
-            <div>{current_question.question}</div>
+            <div>{questions[quesNo].question}</div>
 
-            <div className="options">
-                {options}
-            </div>
+        {
+          questions[quesNo].options.map((option,index)=>{
+            return (
+          <div className="option" key={index} onClick={()=>{ setOptionNo(index)}}>  
+          {option}  
+          </div>
+          )})
+      }
 
             <button onClick={handleSubmit}>Next</button>
         </div>        
+  ) : (
+    <div className='score'>
+    <div>Quiz Completed</div>
+    <div>You scored {score} out of 2!</div>
+    </div>
   )
+    }
+    </>
+    )
 }
 
 export default Quiz
